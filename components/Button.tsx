76a82c5a00391aa822e9f7bcab150d7fa07b35ae
DivyanshuAhirrao <246divyanahuahirrao@gmail.com>
2024-03-05 19:05:32 +0530

    added download app funtionality

--- components/Button.tsx
+++ components/Button.tsx
@@ -6,15 +6,13 @@ type ButtonProps = {
   icon?: string;
   variant: string;
   full?: boolean;
-}
+} & React.ComponentProps<"button">
 
-const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
+const Button = ({ type, title, icon, variant, full,  children, ...rest  }: ButtonProps) => {
   return (
     <button
-    className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}
-      type={type}
-    >
-      {icon && <Image src={icon} alt={title} width={24} height={24} />}
+    className= {`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`} {...rest} type={type}>
+      {icon && <Image src={icon} alt={title} width={24} height={24} />} {children}
       <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
     </button>
   )
